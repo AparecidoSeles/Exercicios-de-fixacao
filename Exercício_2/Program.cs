3b046b558ef3d52243ac7cd511730f5085c5e265
junior <you@example.com>
2020-11-24 16:14:38 -0300

    exercicio 2 pela metade

---
+++ Exercício_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercício_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício 2");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("A - Álcool");
+            Console.WriteLine("G - Gasolina");
+            string combustivel = Console.ReadLine();
+
+            Console.WriteLine("Quantidade a quantidade de combustível");
+            float quantidadecombustivel = float.Parse(Console.ReadLine());
+
+            float percDesconto , desconto, valortotal;
+            float precoAlcool = 4.9f;
+            float precoGasolina = 5.3f;
+
+            
+
+        }
+    }
+}
