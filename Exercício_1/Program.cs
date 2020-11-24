28052dfd3efeada96a401c8fb8022205b7971b42
junior <you@example.com>
2020-11-24 11:33:15 -0300

    exercicio 1

---
+++ Exercício_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercício_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exerecício 1");
+            Console.WriteLine("---------------------");
+
+            Console.WriteLine("Digite o ano atual");
+            int anoAtual = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o ano em que nasceu");
+            int anoNascimento = int.Parse(Console.ReadLine());
+
+            int idade = anoAtual-anoNascimento;
+
+            Console.WriteLine("Você tem "+idade);
+            if (idade>16)
+            {
+                Console.WriteLine("Você já pode votar");
+            }
+            else
+            {
+               Console.WriteLine("Você ainda não pode votar");
+            }
+            
+        }
+    }
+}
