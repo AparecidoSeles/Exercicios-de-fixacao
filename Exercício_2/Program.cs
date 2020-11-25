a27861f54b5cc419e979c4f7af1a5750755b76b6
junior <you@example.com>
2020-11-25 12:38:03 -0300

    exercicio 2 concluido

--- Exercício_2/Program.cs
+++ Exercício_2/Program.cs
@@ -13,12 +13,42 @@ namespace Exercício_2
             string combustivel = Console.ReadLine();
 
             Console.WriteLine("Quantidade a quantidade de combustível");
-            float quantidadecombustivel = float.Parse(Console.ReadLine());
+            float quantidadeCombustivel = float.Parse(Console.ReadLine());
 
-            float percDesconto , desconto, valortotal;
+            float percDesconto , desconto, valorTotal;
             float precoAlcool = 4.9f;
             float precoGasolina = 5.3f;
 
+            switch (combustivel.ToLower())
+            {
+                case "a":
+                if(quantidadeCombustivel>20){
+                    percDesconto = 0.05f;
+                }else{
+                    percDesconto = 0.03f;
+                }
+                    valorTotal = (quantidadeCombustivel * precoAlcool);
+                    desconto = (quantidadeCombustivel * precoAlcool) * percDesconto;
+                    Console.WriteLine($"Seu desconto foi de R${desconto}");
+                    Console.WriteLine($"Valor a ser pago{valorTotal-desconto}");
+                    break;
+
+                case "g":
+                    if(quantidadeCombustivel>20){
+                        percDesconto = 0.06f;
+                    }else{
+                        percDesconto = 0.04f;
+                    }
+                        valorTotal = (quantidadeCombustivel * precoGasolina);
+                    desconto = (quantidadeCombustivel * precoGasolina) * percDesconto;
+                    Console.WriteLine($"Seu desconto foi de R${desconto}");
+                    Console.WriteLine($"Valor a ser pago{valorTotal-desconto}");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção Inválida");
+                    break;
+            }
             
 
         }
