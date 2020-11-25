799f8f4e4f297867cdade48c0846db47f2a5de41
junior <you@example.com>
2020-11-25 15:15:04 -0300

    exercicio 4 finalizado

---
+++ Exercício_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercício_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício 4");
+            Console.WriteLine();
+            Console.WriteLine("Tabuada do 1 Ao 10");
+
+            for (var e = 0; e <=10; e++)
+            {
+                for (var d = 0; d <=10; d++)
+                {
+                    Console.WriteLine( + e +" x "+ d +" = "+e * d);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
